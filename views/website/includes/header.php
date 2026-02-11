583280e526cd8fef3c7bc08ae9f8ee146b830255
swapnalirudratech <swapnalirudratech@gmail.com>
2026-02-11 10:47:54 +0530

    first

--- views/website/includes/header.php
+++ views/website/includes/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="/public/assets/images/logobg.jpg" type="image/x-icon">
+    <link rel="icon" href="/public/assets/images/logo-fav.jpg" type="image/x-icon">
     <title>Animocare</title>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/2.3.0/alpine.js"></script>
